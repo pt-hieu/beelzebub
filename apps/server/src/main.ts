7962e338457fa916bd00a769ac627c30b9201688
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-08-17 11:17:05 +0700

    fix: resolve module

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,12 +5,14 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   )
+
   await app.listen(8000)
 }
 
